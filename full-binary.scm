4a87075f6b400bb16a68a6e3c50a1aa462a4c859
william j lewis <william.lewis112358@gmail.com>
2019-05-30 13:44:52 -0400

    Extract macros into separate file
    In order for the macros to work after the file was first loaded, they
    needed to be placed above their first usage. This cluttered the main
    file.

--- full-binary.scm
+++ full-binary.scm
@@ -3,6 +3,8 @@
 ;; | pebbles?                                                                 |
 ;; +--------------------------------------------------------------------------+
 
+(load "syntax.scm")
+
 ;; all-configs : (Nat, Nat) -> [Tree]
 ;; `all-pebbleable` enumerates all (labeled) full binary trees of a given
 ;; depth that are pebbleable (see below).
@@ -64,28 +66,6 @@
 ;; `Inner` constructs an interior vertex.
 (define (Inner top left right) (list top left right))
 
-;; `with-tree` provides some additional syntax for working with binary trees.
-;; You provide a tree, an expression to be evaluated in the case that it is a
-;; leaf vertex, and an expression to be evaluated in the case that it is an
-;; interior vertex. Additionally, the value of the leaf vertex, and of the
-;; root vertex, left subtree, and right subtree, are bound to variables that
-;; you supply before the respective expressions. For instance,
-;;   (with-tree (Inner 3 (Leaf 0) (Leaf 1))
-;;     (Leaf (v)      `(a leaf whose value is ,v))
-;;     (Inner (t l r) `(an inner vertex whose left-subtree is ,l)))
-;;   => (an inner vertex whose left subtree is 0)
-(define-syntax with-tree
-  (syntax-rules (Leaf Inner)
-    ((with-tree tree
-       (Leaf (v) leaf-expr)
-       (Inner (t l r) inner-expr))
-     (if (leaf? tree)
-       ((lambda (v) leaf-expr) (leaf-value tree))
-       ((lambda (t l r) inner-expr)
-        (tree-top tree)
-        (tree-left tree)
-        (tree-right tree))))))
-
 (define leaf? number?)
 (define (leaf-value l) l)
 
@@ -114,17 +94,3 @@
 ;; return : a -> [a]
 ;; `return` is the monadic "return"/"pure" operation for lists.
 (define (return n) (list n))
-
-;; `list-comp` provides some new syntax for rudimentary list-comprehensions,
-;; in the hope of cleaning expressions involving a lot of >>='s.
-;; For example,
-;;   (list-comp `(,x ,y) (<- x '(1 2 3)) (<- y '(a b)))
-;;   => '((1 a) (1 b) (2 a) (2 b) (3 a) (3 b))
-(define-syntax list-comp
-  (syntax-rules (<-)
-    ((_ expr) expr)
-    ((_ expr (<- x gen))
-     (>>= gen (lambda (x) (return expr))))
-    ((_ expr (<- x gen) (<- xs gens) ...)
-     (>>= gen (lambda (x)
-      (list-comp expr (<- xs gens) ...))))))
